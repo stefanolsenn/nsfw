a6879a19c865c91c74874e4271b9ef05fff223b6
Stefan <so@tutee.dk>
2018-12-02 01:23:09 +0100

    u

--- main.go
+++ main.go
@@ -32,13 +32,10 @@ func imgHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, fmt.Sprintf("Error decoding body: %v", fmt.Sprint(err)))
 	}
-	imgUrl := r.URL.Path[1:]
-	if len(imgUrl) > 2 {
-		cmd := exec.Command("python", "/workspace/classify_nsfw.py", "--model_def", "/workspace/nsfw_model/deploy.prototxt", "--pretrained_model", "/workspace/nsfw_model/resnet_50_1by2_nsfw.caffemodel", fmt.Sprint(img.Path))
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Fprintf(w, fmt.Sprintf("error in Output():", fmt.Sprint(err)))
-		}
-		fmt.Fprintf(w, fmt.Sprintf("Result: %v", string(output)))
+	cmd := exec.Command("python", "/workspace/classify_nsfw.py", "--model_def", "/workspace/nsfw_model/deploy.prototxt", "--pretrained_model", "/workspace/nsfw_model/resnet_50_1by2_nsfw.caffemodel", fmt.Sprint(img.Path))
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("error in Output():", fmt.Sprint(err)))
 	}
+	fmt.Fprintf(w, fmt.Sprintf("Result: %v", string(output)))
 }
